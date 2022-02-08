3b29eaf10bec7de0ae6197443918306e7aeff056
Chirag Goyal <99237546+cgagarwal@users.noreply.github.com>
2022-02-08 11:06:58 +0530

    Create L1 - Introduction.c

---
+++ P1 - Introduction to C/L1 - Introduction.c
@@ -0,0 +1,28 @@
+// pre-processor command to include standard library
+#include <stdio.h>
+
+// warning generated : default type 'int'
+main() {
+
+    /*
+               ---------------- Process in System ----------------
+    preprocessing ----> compilation ---> assembly ---> linking -----> loading
+        
+        
+       ------------------------- Rules for Variable Declaration ------------------------------
+    --- A variable can have alphabets, digits, and underscore.
+    --- A variable name can start with the alphabet, and underscore only. It can't start with a digit.
+    --- No whitespace is allowed within the variable name.
+    --- A variable name must not be any reserved word or keyword.
+    
+    */
+    
+    // input from user
+    int a;
+    scanf("%d", &a);
+ 
+    // print the statement
+    printf("Hello , Chirag\n");
+    
+    return 0;       // Not Must
+}
